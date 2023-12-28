93ee39db373d83f3d7d24ddea728a6f61e5681fa
Shoko <97951985+Shokoloa@users.noreply.github.com>
2023-12-28 14:10:48 +0100

    Add files via upload

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,1 +1,8 @@
-
+export const Background = () => (
+  <div className="bg">
+    <div className="line"></div>
+    <div className="line"></div>
+    <div className="line"></div>
+    <img src="https://desktop.github.com/images/star-bg.svg" draggable={false} alt="" />
+  </div>
+);
