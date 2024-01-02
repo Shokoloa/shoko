d06d766cd79b1478148986d4c697212af83fc91e
Shoko <97951985+Shokoloa@users.noreply.github.com>
2024-01-02 15:45:10 +0100

    Update console.js

--- app/js/console.js
+++ app/js/console.js
@@ -1,1 +1,8 @@
+function consoleMessage() {
+    let time = performance.timing;
+    let pageloadtime = time.loadEventStart - time.navigationStart;
 
+    console.log("Loaded in " + pageloadtime + "ms")
+}
+
+window.addEventListener('load', consoleMessage);
