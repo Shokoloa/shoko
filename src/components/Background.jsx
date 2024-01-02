d5e2777c566f9eba7159ed0735de5aa6f4a8d668
Shoko <97951985+Shokoloa@users.noreply.github.com>
2024-01-02 15:38:24 +0100

    Delete src directory

--- src/components/Background.jsx
+++
@@ -1,8 +0,0 @@
-export const Background = () => (
-  <div className="bg">
-    <div className="line"></div>
-    <div className="line"></div>
-    <div className="line"></div>
-    <img src={'https://desktop.github.com/images/star-bg.svg'} draggable={false} alt="" />
-  </div>
-);
